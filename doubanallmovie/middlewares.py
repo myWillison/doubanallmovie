3bf48522ef95e185bc34c2a355795226c523c54c
tearsforyears <1027664894@qq.com>
2018-09-17 20:39:06 +0800

    scrapy to crawl all comment in douban

---
+++ doubanallmovie/middlewares.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your spider middleware
+#
+# See documentation in:
+# https://doc.scrapy.org/en/latest/topics/spider-middleware.html
+
+from scrapy import signals
+import requests as req
+from requests.exceptions import ConnectionError
+import logging
+import random
+class RadomUserAgent(object):
+    logger = logging.getLogger(__name__)
+    random_ualist=[
+        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/22.0.1207.1 Safari/537.1",
+        "Mozilla/5.0 (X11; CrOS i686 2268.111.0) AppleWebKit/536.11 (KHTML, like Gecko) Chrome/20.0.1132.57 Safari/536.11",
+        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.6 (KHTML, like Gecko) Chrome/20.0.1092.0 Safari/536.6",
+        "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.6 (KHTML, like Gecko) Chrome/20.0.1090.0 Safari/536.6",
+        "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/19.77.34.5 Safari/537.1",
+        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/536.5 (KHTML, like Gecko) Chrome/19.0.1084.9 Safari/536.5",
+        "Mozilla/5.0 (Windows NT 6.0) AppleWebKit/536.5 (KHTML, like Gecko) Chrome/19.0.1084.36 Safari/536.5",
+        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1063.0 Safari/536.3",
+        "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1063.0 Safari/536.3",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_0) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1063.0 Safari/536.3",
+        "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1062.0 Safari/536.3",
+        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1062.0 Safari/536.3",
+        "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3",
+        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3",
+        "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3",
+        "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.0 Safari/536.3",
+        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/535.24 (KHTML, like Gecko) Chrome/19.0.1055.1 Safari/535.24",
+        "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/535.24 (KHTML, like Gecko) Chrome/19.0.1055.1 Safari/535.24"
+    ]
+    def get_random_Agent(self):
+        return random.choice(self.random_ualist)
+    def process_request(self, request, spider):
+        self.logger.debug("use random user-agent,")
+        request.headers['User-Agent'] = self.get_random_Agent()
+    #'scrapy crawl somespider -s JOBDIR=crawls/somespider-1'
+class DoubanDownloaderMiddleware(object):
+    logger=logging.getLogger(__name__)
+    def get_proxy(self):
+        try:
+            resp=req.get('http://127.0.0.1:5010/get/')
+            return 'http://'+resp.text
+        except ConnectionError:
+            self.logger.debug("use proxy faild")
+            raise ConnectionError
+    def process_request(self, request, spider):
+        proxy=self.get_proxy()
+        self.logger.debug("use proxy "+str(proxy))
+        request.meta['proxy'] = proxy
+        return None
---
+++ doubanallmovie/spiders/movie.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+from doubanallmovie.items import movieItem,commentItem
+from scrapy import Spider,Request
+class MovieSpider(Spider):
+    name = 'movie'
+    allowed_domains = ['douban.com']
+    start_urls = ['https://movie.douban.com/subject/1300300/','https://movie.douban.com/subject/26861685/','https://movie.douban.com/subject/26644205/','https://movie.douban.com/subject/26684564/']#从一个电影开始
+
+    def start_requests(self):
+        for url in self.start_urls:
+            yield Request(url=url,callback=self.parse_index)
+    def parse_index(self, response):
+        #list数据
+        performers = response.css('#info > span.actor > span.attrs a::text').extract()
+        #单变量数据
+        title = response.css('#content > h1 > span:nth-child(1)::text').extract_first()
+        time = response.css('#content > h1 > span.year::text').extract_first()
+        diretor = response.css('#info > span:nth-child(1) > span.attrs > a::text').extract_first()
+        screen_writer = response.css('#info > span:nth-child(3) > span.attrs a::text').extract_first()
+        movie_type1 = response.css('#info > span:nth-child(8)::text').extract_first()
+        movie_type2 = response.css('#info > span:nth-child(9)::text').extract_first()
+        movie_length = response.css('#info > span:nth-child(20)::text').extract_first()
+        score = response.css('#interest_sectl > div.rating_wrap.clearbox > div.rating_self.clearfix > strong::text').extract_first()
+        people_num = response.css('#interest_sectl > div.rating_wrap.clearbox > div.rating_self.clearfix > div > div.rating_sum > a > span::text').extract_first()
+        rate5 = response.css('#interest_sectl > div.rating_wrap.clearbox > div.ratings-on-weight > div:nth-child(1) > span.rating_per::text').extract_first()
+        rate4 = response.css('#interest_sectl > div.rating_wrap.clearbox > div.ratings-on-weight > div:nth-child(2) > span.rating_per::text').extract_first()
+        rate3 = response.css('#interest_sectl > div.rating_wrap.clearbox > div.ratings-on-weight > div:nth-child(3) > span.rating_per::text').extract_first()
+        rate2 = response.css('#interest_sectl > div.rating_wrap.clearbox > div.ratings-on-weight > div:nth-child(4) > span.rating_per::text').extract_first()
+        rate1 = response.css('#interest_sectl > div.rating_wrap.clearbox > div.ratings-on-weight > div:nth-child(5) > span.rating_per::text').extract_first()
+
+        # items = movieItem() #主页面的数据结构
+        #for field in items.fields:
+        #    items[field]=eval(field)#动态存储
+        #yield items
+
+        comment_url = response.url + 'comments?start={start}&limit={limit}'#评论页面
+        for start in range(0,220,20):#这是无登陆
+            yield Request(url=comment_url.format(start=start,limit=20),callback=self.parse_comments,meta={'title':title})
+        new_urls = response.css('#recommendations > div dl dt a::attr(href)').extract()#推荐电影 [:-18]
+        for url in new_urls:
+            yield Request(url=url[:-18],callback=self.parse_index)
+    def parse_comments(self,response):
+        items2 = commentItem()
+        temp = response.css('#comments div.comment-item div.comment h3 span.comment-info')
+        usernames = temp.css('a::text').extract()
+        scores = temp.css('span:nth-child(3)::attr(title)').extract()
+        times = temp.css('span:nth-child(4)::text').extract()
+        comments = response.css('#comments div.comment-item div.comment p span.short::text').extract()
+        votes = response.css('#comments div.comment-item div.comment h3 span.comment-vote span.votes::text').extract()
+        #print(usernames,scores,times,comments,votes)
+        title = response.meta['title']#从前一个页面得到的
+        items2['movie_name'] = title
+        for i in range(len(times)):
+            items2['username']=usernames[i]
+            items2['score'] = scores[i]
+            items2['time'] = times[i]
+            items2['comment'] = comments[i]
+            items2['vote'] = votes[i]
+            yield items2
